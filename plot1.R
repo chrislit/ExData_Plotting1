54079bb4efdb42a899d33370fa330f25d12917d9
Chris Little <chrisclittle@gmail.com>
2015-05-10 01:59:37 -0700

    updated to conform to plot2 code

--- plot1.R
+++ plot1.R
@@ -3,12 +3,14 @@ hpc.df <- read.table(file = "household_power_consumption.txt",
                      sep = ";", header = TRUE, na.strings = "?")
 
 # Convert the date col to Date objects
-hpc.df$Date <- as.Date(hpc.df$Date, format = "%d/%m/%Y")
+hpc.df$DateTime <- strptime(paste(hpc.df$Date, hpc.df$Time),
+                            format="%d/%m/%Y %H:%M:%S")
 
 # Subset to data between 2007-02-01 & 2007-02-02
-hpc.df <- hpc.df[hpc.df$Date >= as.Date("2007-02-01") &
-                   hpc.df$Date <= as.Date("2007-02-02"),]
+hpc.df <- hpc.df[hpc.df$DateTime >= as.POSIXlt("2007-02-01") &
+                   hpc.df$DateTime < as.POSIXlt("2007-02-03"),]
 
+# Create the histogram
 hist(hpc.df$Global_active_power,
      xlab="Global Active Power (kilowatts)",
      col="red",
